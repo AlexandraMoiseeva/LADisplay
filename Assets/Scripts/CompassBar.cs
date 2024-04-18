9233960acd2350d5d1d71354b3a579d7cd9a4b16
AlexandraMoiseeva <60811254+AlexandraMoiseeva@users.noreply.github.com>
2024-04-18 18:08:02 +0300

    ver2.6beta

--- Assets/Scripts/CompassBar.cs
+++ Assets/Scripts/CompassBar.cs
@@ -12,16 +12,12 @@ public class CompassBar : MonoBehaviour
 
     void Start()
     {
-        begin = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         //compassImage.uvRect = new Rect(camera.localEulerAngles.y / 360f, 0f, 1f, 1f);
-        compassImage.uvRect = new Rect(begin/1000, 0f, 1f, 1f);
-        begin += 1;
-        if (begin == 1000)
-            begin = 0;
+        compassImage.uvRect = new Rect(0f, (180.0f + (Data.pitch) / System.MathF.PI * 180.0f)/360f, 1f, 1f);
     }
 }
